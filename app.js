d928a132bf8df9e55799e9a0e869765c55ad310f
Oroko <aoroko09@gmail.com>
2021-10-11 01:35:45 -0400

    Replaced hard coded data with data from the API

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-const express = require('express')
+import express from 'express'
 const app = express() 
 const port = 3000 
 
 // middleware 
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 
 // Import Route 
-const weatherRoute = require('./routes/weather')
+import router from './routes/weather.js'
 
 // Use view Engine 
 app.set('view engine', 'ejs')
 
 // Middleware route 
-app.use('/', weatherRoute)
+app.use('/', router)
 app.listen(port, () => {
   console.log(`Server starting at http://localhost:${port}`)
 })
--- routes/weather.js
+++ routes/weather.js
@@ -1,5 +1,56 @@
-const router = require('express').Router()
-router.get("/", (req,res) => {
-  res.render('index')
-})
-module.exports = router
+import fetch from "node-fetch";
+import dotenv from 'dotenv'
+import express from 'express'
+const router = express.Router()
+
+dotenv.config();
+router.get("/", (req, res) => {
+  res.render("index", {
+    city: null,
+    description: null,
+    icon: null,
+    temp: null,
+  });
+});
+
+router.post("/", async (req, res) => {
+  const city = req.body.city;
+  
+  const url_api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+  try {
+    await fetch(url_api)
+      .then((res) => res.json())
+      .then((data) => {
+        if(data.message === 'city not found'){
+          res.render('index', {
+            city: data.message,
+            description: null,
+            icon: null,
+            temp: null
+          })
+        }else{
+          const city = data.name 
+          const description = data.weather[0].description
+          const temp = data.main.temp 
+          const icon = data.weather[0].icon
+          res.render('index', {
+            city,
+            description,
+            icon,
+            temp
+          })
+        }
+      });
+  }catch{
+    res.render("index", {
+      city: "Something went wrong",
+      description: null,
+      icon: null,
+      temp: null,
+    });
+  }
+
+
+});
+//module.exports = router;
+export default router
