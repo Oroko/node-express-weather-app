4906513cdc528956e1a64d7ba45fcb3751ba3326
Oroko <aoroko09@gmail.com>
2021-10-10 23:43:32 -0400

    Configured app to use static css files

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express() 
 const port = 3000 
 
+// middleware 
+app.use(express.static('public'))
+
 // Import Route 
 const weatherRoute = require('./routes/weather')
 
