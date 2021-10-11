96c344d9df0b5d1ecc3d0fa434c849c311a041a6
Oroko <aoroko09@gmail.com>
2021-10-10 21:35:59 -0400

    Install EJS

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const port = 3000
 // Import Route 
 const weatherRoute = require('./routes/weather')
 
+// Use view Engine 
+
+
 // Middleware route 
 app.use('/', weatherRoute)
 app.listen(port, () => {
